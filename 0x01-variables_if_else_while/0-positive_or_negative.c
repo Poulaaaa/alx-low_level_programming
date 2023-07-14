ac89307c802a46e5b170b2ca87505323035cffb1
Poula <poulaa.abd.elmasseh@gmail.com>
2023-07-14 18:34:25 +0300

    last adjust

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,14 +9,16 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0) {
-		    printif("%i is positive. \n" ,n);
+	if (n > 0)
+	{
+		printf("%i is positive. \n", n);
 	}
-	else if (n < 0) {
-		    printif("%i is negative. \n" ,n);
+	else if (n < 0)
+	{
+		printf("%i is negative. \n", n);
 	}
-	else {
-		    printif("%i is zero. \n" ,n);
+	else
+	{
+		printf("%i is zero. \n", n);
 	}
-	
 	return (0);
